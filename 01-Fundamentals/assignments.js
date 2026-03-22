a370c00c7bbca2ee2500eb590053bbd62bfc9e87
Yoggie28 <george.panagiotakopoulos28@gmail.com>
2026-03-22 21:19:17 +0200

    Done some ass 01

---
+++ 01-Fundamentals/assignments.js
@@ -0,0 +1,31 @@
+/*let country = "Greece"
+const continent = "Europe"
+let population = 10000
+population ++;
+
+const isIsland = false;
+const language = "Greek";
+
+console.log(isIsland, population, country , language)
+
+let description = `${country} is in ${continent}, and its ${population}million people speak ${language}}`
+console.log(description)
+
+if (population > 330000){
+    console.log(`${country} population is above average`)
+
+}else {
+    console.log(`${country} population is below average`)
+}
+
+let numNeigbours = Number(prompt("How many neigbour countries does your country have ?"));
+
+if (numNeigbours === 1){
+    console.log("Only 1 boarder")
+}else if (numNeigbours > 1){
+    console.log("More than 1 boarder")
+}else {
+    console.log("No borders")
+}
+*/
+
