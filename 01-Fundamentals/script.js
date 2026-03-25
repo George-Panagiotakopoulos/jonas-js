d560cbbc43b2cee13825f12d664d9d6807823e63
Yoggie28 <george.panagiotakopoulos28@gmail.com>
2026-03-25 21:03:13 +0200

    Coding Challenge #3

--- 01-Fundamentals/script.js
+++ 01-Fundamentals/script.js
@@ -15,7 +15,7 @@ m tall.
 § Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76
 m tall.
 GOOD LUCK 😀 */
-
+/*
 let markMass = 78;
 let markHeight = 1.95;
 let johnMass = 92;
@@ -26,10 +26,26 @@ johnBmi = johnMass / johnHeight ** 2 ;
 markHigherBMI = markBmi < johnBmi;
 //console.log (`Marks BMI is ${MarkBmi.toFixed(2)},and Johns is ${JohnBmi.toFixed(2)},is Johns BMI higher?
 //${markHigherBMI}`);
+
+
 //CODING CHALLENGE #2
-if (markBmi > johnBmi){
+
+/* if (markBmi > johnBmi){
     console.log(`Marks Bmi${markBmi.toFixed(2)} is Higher than John's ${johnBmi.toFixed(2)}`)
 }else {
     console.log(`John's BMI ${johnBmi.toFixed(2)} is higher than Marks ${markBmi.toFixed(2)}`)
-}
+} */
 
+
+// CODING CHALLENGE #3
+
+let dolpins = (96 + 108 + 89) / 3
+let coalas = (88 + 91 + 110) / 3
+
+if (dolpins > coalas && dolpins >=100) {
+    console.log("Dolphins Win")
+}else if (dolpins < coalas && coalas >=100) {
+    console.log("Coalas Win")
+} else {
+    console.log("It's a draw")
+}
